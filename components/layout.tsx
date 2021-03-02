058c19d0d237e57f00670d5b09de4a173b5b9f8d
Oleg Krotenko <olk@stylelabs.com>
2021-03-02 04:14:25 +0200

    console ver

--- components/layout.tsx
+++ components/layout.tsx
@@ -9,6 +9,7 @@ export function Layout({ children }) {
                 <link rel="icon" href="./favicon.ico" />
                 <meta name="description" content="Новые сервера л2. Бесплатный анонс серверов Lineage 2 всех рейтов и хроник. Самая большая база серверов. Не пропустите открытие серверов l2 уже сегодня!" />
                 <meta name="keywords" content="Новые сервера Lineage 2, анонсы серверов Lineage 2, новые сервера л2, анонсы л2, л2, ла2, la2, l2, lineage ii, линейдж 2, линейка, l2 anons, сервера l2" />
+                <meta name="google-site-verification" content="tr2jipBoV-jJZgiqKUAUUs_Rh_ozGNsBu6jXu-nctZc" />
             </Head>
             <NavBar />
             <main className="container mx-auto flex-row mt-5">
